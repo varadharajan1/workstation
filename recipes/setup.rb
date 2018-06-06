2d0b25769910097e63232e64908859f4c7a569af
vagrant <vagrant@localhost.localdomain>
2018-06-06 17:53:04 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,25 @@
+package 'vim-enhanced'
+
+package 'nano'
+
+package 'tree' do
+ action :install
+end
+
+package 'git' do
+ action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+ content 'This server is the property of TechnoTraininer'
+ action :create
+ owner  'root'
+ group  'root'
+end
+
+service 'ntpd' do
+ action [ :enable, :start ]
+end
+
